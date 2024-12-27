333463c9375f5903ab6ae0211c89c475bea93f2d
necrashter <iiilker99@gmail.com>
2024-12-27 14:31:16 +0300

    Add battery icon

--- Menu.js
+++ Menu.js
@@ -14,6 +14,7 @@ var attributionHTML = `
   Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
   Footprint:
   <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  <a href="https://www.flaticon.com/free-icons/battery" title="battery icons">Battery icons created by Pixel perfect - Flaticon</a>
 
   Leaflet
   https://github.com/bbecquet/Leaflet.PolylineDecorator
